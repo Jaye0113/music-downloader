d9819d2dd58517617b21fca513d9230addb52234
Yaoyao Liu <970739985jl@gmail.com>
2024-02-05 21:50:17 -0800

    Update download_music.sh

--- download_music.sh
+++ download_music.sh
@@ -10,22 +10,4 @@ fi
 cd ~/Downloads
 
 # Use yt-dlp to download music file with fixed temple of output file
-yt-dlp -x $1
-
-# get the file name with .opus
-OPUS_FILE=$(ls -t | grep '\.opus$' | head -n 1)
-
-if [ ! -z "$OPUS_FILE" ]; then
-	# Remove the name of extension and ready to transfer
-	BASE_NAME="$(OPUS_FILE%.*)"
-
-	# Use ffmpeg to transfer .opus into .mp3
-	ffmpeg -i "$OPUS_FILE" "$BASE_NAME.mp3"
-
-	# Delete the original .opus file
-	# rm "$OPUS_FILE"
-
-	echo "Completed!"
-else
-	echo "There is no .opus file, so be skipped."
-fi
+yt-dlp -x --audio-format mp3 $1
